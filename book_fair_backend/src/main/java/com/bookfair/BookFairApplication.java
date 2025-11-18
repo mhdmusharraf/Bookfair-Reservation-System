4998299f9dec058590a3fd98a84fb606545427a8
FarhathCader <farhadfd818@gmail.com>
2025-11-19 02:35:53 +0530

    import issue fixed

--- book_fair_backend/src/main/java/com/bookfair/BookFairApplication.java
+++ book_fair_backend/src/main/java/com/bookfair/BookFairApplication.java
@@ -1,5 +1,4 @@
 package com.bookfair;
-//check
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
